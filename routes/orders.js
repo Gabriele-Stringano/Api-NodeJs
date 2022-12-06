086434b81b05f1cdccfd5d7b0716764648593d82
Gabriele-Stringano <gabrielestringano@gmail.com>
2022-12-06 22:11:05 +0100

    Added orders functionality
    It's possible to create/edit/search/delete orders that contain arrays of Users and Products

--- routes/orders.js
+++ routes/orders.js
@@ -2,13 +2,19 @@ const express = require("express");
 const router = express.Router()
 const {
     orderList,
+    orderById,
     ordersByDate,
     ordersByProduct,
     addOrder,
     deleteOrder,
+    updateOrder,
+    updateOrderAdd,
+    updateOrderRemove,
 } = require("../utils/orderUtils")
 
 router.get('/',orderList )
+
+router.get('/:orderId',orderById )
 
 router.get('/by-date/:date',ordersByDate)
 
@@ -17,5 +23,11 @@ router.get('/by-product/:product',ordersByProduct)
 router.post('/',addOrder)
 
 router.delete( "/:orderId", deleteOrder)
+
+router.put("/:orderId", updateOrder)
+
+router.put("/add-infos/:orderId", updateOrderAdd)
+
+router.put("/remove-infos/:orderId", updateOrderRemove)
 
 module.exports = router
--- utils/orderUtils.js
+++ utils/orderUtils.js
@@ -1,5 +1,6 @@
 const Order = require ('../models/orders')
 const Product = require ('../models/products')
+const User = require ('../models/users')
 const {validateOrder,validateOrderDates} = require ('../validators/order')
 
 const orderList = async (req, res) => {
@@ -12,12 +13,22 @@ const orderList = async (req, res) => {
     }
   }
 
+  const orderById = async (req, res) => {
+    try {
+      const order = await Order.findById(req.params.orderId);
+      if (!order) throw "not found";
+      res.status(200).json(order);
+    } catch (err) {
+      res.status(404).json({ message: err });
+    }
+  }
+
   const ordersByDate = async (req, res) => {
     try {
       const datetarget = req.params.date
      const orders = await Order.find( { date: datetarget  });
      // print a message if no documents were found 
-     if(await orders.length < 1) throw "not found";
+     if(orders.length < 1) throw "not found";
       res.status(200).json(orders);
     } catch (err) {
       res.status(404).json({ message: err });
@@ -29,7 +40,7 @@ const orderList = async (req, res) => {
       const productTarget = req.params.product
      const orders = await Order.find( { products: productTarget }); 
      // print a message if no documents were found 
-     if(await orders.length < 1) throw "not found";
+     if(orders.length < 1) throw "not found";
       res.status(200).json(orders);
     } catch (err) {
       res.status(404).json({ message: err });
@@ -42,16 +53,33 @@ const orderList = async (req, res) => {
     return res.status(422).json({ message: error.details })
    }
 
+   let check
+   let elementChecked
+
     let products = await Product.find()
-    let check
-    req.body.products.forEach( (element) => { 
-        check = false 
+    req.body.products.every( (element) => {
+      elementChecked = element
+      check = false
         products.forEach( (product) => {
            if(element == product._id){ check = true}
         })
+      if(check == false) return check
+      return check
     })
-    if(check == false){ return res.status(422).json({ message: 'id non validp' }) }
-        
+    if(check == false){ return res.status(422).json({ message: 'id prodotto non valido ' + elementChecked }) }
+
+    let users = await User.find()
+    req.body.users.every( (element) => { 
+      elementChecked = element
+      check = false
+          users.forEach( (user) => {
+           if(element == user._id){ check = true}
+        })
+      if(check == false) return check
+      return check
+    })
+    if(check == false){ return res.status(422).json({ message: 'id user non valido ' + elementChecked }) }
+
         const order = new Order({
         products: req.body.products,
         users:req.body.users,
@@ -74,10 +102,114 @@ const orderList = async (req, res) => {
     }
   }
 
+  const updateOrder = async (req, res) => {
+    const {error, value} = validateOrderDates(req.body)
+    if(error){
+      return res.status(422).json({ message: error.details })
+     } 
+    try {
+      const updatedOrder = await Order.updateOne(
+        { _id: req.params.orderId },
+        {
+          $set: {
+            products: req.body.products,
+            users:req.body.users,
+            date: req.body.date
+          },
+        }
+      );
+      res.status(200).json(updatedOrder);
+    } catch (err) {
+      res.status(404).json({ message: err });
+    }
+  }
+
+  const updateOrderAdd = async (req, res) => {
+    const {error, value} = validateOrderDates(req.body)
+    if(error){
+      console.log(error)
+      return res.status(422).json({ message: error.details })
+     } 
+    try {
+      const order = await Order.findById(req.params.orderId);
+
+      let temporalProducts = order.products
+      if (typeof(req.body.products) != "undefined"){
+        req.body.products.forEach( (product) => {
+          temporalProducts.push(product)
+        })
+      }
+
+      let temporalUsers = order.users
+      if (typeof(req.body.users) != "undefined"){
+        req.body.users.forEach( (user) => {
+          temporalUsers.push(user)
+        })
+      }
+
+      const updatedOrder = await Order.updateOne(
+        { _id: req.params.orderId },
+        {
+          $set: {
+            products: temporalProducts,
+            users:temporalUsers,
+            date: req.body.date
+          },
+        }
+      );
+      res.status(200).json(updatedOrder);
+    } catch (err) {
+      res.status(404).json({ message: err });
+    }
+  }
+
+  const updateOrderRemove = async (req, res) => {
+    const {error, value} = validateOrderDates(req.body)
+    if(error){
+      console.log(error)
+      return res.status(422).json({ message: error.details })
+     } 
+    try {
+      const order = await Order.findById(req.params.orderId);
+
+      let temporalProducts = order.products
+      if (typeof(req.body.products) != "undefined"){
+        req.body.products.forEach( (product) => {
+          temporalProducts = temporalProducts.filter( element => element != product)
+        })
+      }
+
+      let temporalUsers = order.users
+      if (typeof(req.body.users) != "undefined"){
+        req.body.users.forEach( (user) => {
+          temporalUsers = temporalUsers.filter( element => element != user)
+        })
+      }
+
+      const updatedOrder = await Order.updateOne(
+        { _id: req.params.orderId },
+        {
+          $set: {
+            products: temporalProducts,
+            users:temporalUsers,
+            date: req.body.date
+          },
+        }
+      );
+      res.status(200).json(updatedOrder);
+    } catch (err) {
+      res.status(404).json({ message: err });
+    }
+  }
+
   module.exports = {
     orderList,
+    orderById,
     ordersByDate,
     ordersByProduct,
     addOrder,
     deleteOrder,
+    updateOrder,
+    updateOrderAdd,
+    updateOrderRemove,
   }
